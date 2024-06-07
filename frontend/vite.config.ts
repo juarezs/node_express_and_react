a98a0da76cd133fa1adff152feae434f922cc375
Juarez Schulz <juarezs@gmail.com>
2024-06-07 03:24:20 -0300

    add api url logging

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+
+  console.info(`api url: ${env.API_BASE_URL}`);
   
   return {
     plugins: [react()],
